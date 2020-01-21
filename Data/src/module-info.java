c25aad503bec6810222c3776a06ae995ba8241b3
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2020-01-21 22:49:23 +0200

    Added task modules

---
+++ Data/src/module-info.java
@@ -0,0 +1,5 @@
+module Data {
+    exports aprilData;
+    exports marchData;
+    requires DataCleaningCode;
+}
--- src/com/cursor/Main.java
+++
@@ -1,9 +0,0 @@
-package com.cursor.hw12;
-
-import com.cursor.hw12.controller.AppExecutor;
-
-public class Main {
-    public static void main(String[] args) {
-        new AppExecutor().runProgram();
-    }
-}
